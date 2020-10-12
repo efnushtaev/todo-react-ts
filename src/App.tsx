d490b97ee70792fd4d9c11ed5d9017c93e56ea9f
efim-nushtaev <efim.nushtaev@simbirsoft.com>
2020-10-12 09:15:00 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Navbar } from './components/navbar/Navbar';
 import { AboutPage } from './pages/AboutPage';
 import { TodoPage } from './pages/TodosPage';
-import { BrowserRouter, Switch, Route, HashRouter} from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
 
-  return <HashRouter>
+  return <BrowserRouter>
     <Navbar />
     <div className="container">
       <Switch>
@@ -14,7 +14,7 @@ const App: React.FC = () => {
         <Route component={AboutPage} path='/about' exact />
       </Switch>
     </div>
-  </HashRouter>
+  </BrowserRouter>
 }
 
 export default App;
